5ebcbb2a1b0f8149bc4fb501d9b8708fbab659ff
Lassi Chen <lachen@utu.fi>
2023-04-12 00:12:11 +0300

    4.10 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,15 +1,19 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
+  const user = await User.findById(blog.user)
 
   const result = await blog.save()
+  user.blogs = user.blogs.concat(result._id)
+  await user.save()
   response.status(201).json(result)
 })
 
