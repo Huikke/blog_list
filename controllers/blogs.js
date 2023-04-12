fb60cd2d4615fd4f2bfc8627b5b6f6717a534943
Lassi Chen <lachen@utu.fi>
2023-04-12 18:49:38 +0300

    4.11, 4.12 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,15 +1,32 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
-  const user = await User.findById(blog.user)
+  
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+  blog["user"] = user
 
   const result = await blog.save()
   user.blogs = user.blogs.concat(result._id)
